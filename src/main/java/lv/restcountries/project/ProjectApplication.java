bef6c7a5d78114aff02b6e04bd205fee3a20406a
Maksim Mitiakin <mitiakin@inbox.lv>
2021-05-08 12:44:45 +0300

    First Commit

---
+++ src/main/java/lv/restcountries/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package lv.restcountries.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lv/restcountries/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.restcountries.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
